e8aecec1d46a3f6c16d4c0a5d030d47b02bfb795
Ronald <ronald.isael@hotmail.com>
2022-11-28 20:13:43 -0400

    Depencies ADD

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
+import JSONPretty from "react-json-pretty";
 
 const Profile = () => {
-  const { user } = useAuth0();
-  console.log(user);
-  
-  return (
-      <div>
-        <h1>Profile</h1> 
-        <p>{JSON.stringify(user)}</p>
-      </div>
-  );
+    const { user } = useAuth0();
+    console.log(user);
+
+    return (
+        <div>
+            <h1>Profile</h1>
+            <p>{JSON.stringify(user)}</p>
+
+            <img src={user.picture} alt={user.name} />
+            <h2>{user.name}</h2>
+            <p>{user.nickname}</p>
+
+            <JSONPretty data={user} />;
+        </div>
+    );
 }
 
 export default Profile;
